4d0001608abfbd316385bc4f0c3671b5654ddfb4
Njoura7 <njourawebdev@gmail.com>
2024-03-31 16:32:56 +0200

    set up dashboard page layout/prepare create match form

---
+++ src/components/CreateMatchDialog.tsx
@@ -0,0 +1,142 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { DatePicker } from "./DatePicker";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+    DialogClose
+} from "@/components/ui/dialog";
+import { Form,FormControl,FormDescription,FormField,FormItem,FormLabel,FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button"; 
+
+const matchFormSchema = z.object({
+    name: z.string().min(5, {
+      message: "Name must be at least 5 characters.",
+    }),
+    code: z.string().min(4, {
+      message: "Code must be at least 4 characters.",
+    }),
+    place: z.string().min(5, {
+      message: "Place must be at least 5 characters.",
+    }),
+    date: z.date(),
+  });
+  
+  interface MatchFormValues {
+    name: string;
+    code: string;
+    place: string;
+    date: Date;
+  }
+
+export function CreateMatchDialog() {
+    const form = useForm({
+        resolver: zodResolver(matchFormSchema),
+        defaultValues: {
+          name: "",
+          code: "",
+          place: "",
+          date: new Date(),
+        },
+      });
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button variant="outline" className="font-bold py-2 px-4 rounded mb-4">
+                    Create Match
+                </Button>
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Create a Match</DialogTitle>
+                    <DialogDescription>
+                        You can create a match by entering the match details below.
+                    </DialogDescription>
+                </DialogHeader>
+                <Form {...form}>
+                    <form  className="space-y-8">
+                        <FormField
+                        control={form.control}
+                        name="name"
+                        render={({ field }) => (
+                            <FormItem className="mx-6">
+                            <FormLabel>Match name</FormLabel>
+                            <FormControl>
+                                <Input placeholder="Enter the match name" {...field} autoComplete="name"/>
+                            </FormControl>
+                            <FormDescription>
+                                This is your public match display name.
+                            </FormDescription>
+                            <FormMessage />
+                            </FormItem>
+                        )}
+                        />
+                        <FormField
+                        control={form.control}
+                        name="code"
+                        render={({ field }) => (
+                            <FormItem className="mx-6">
+                            <FormLabel>Match code</FormLabel>
+                            <FormControl>
+                                <Input placeholder="Enter the match code" {...field} autoComplete="code"/>
+                            </FormControl>
+                            <FormDescription>
+                                This is your match code .
+                            </FormDescription>
+                            <FormMessage />
+                            </FormItem>
+                        )}
+                        />
+                        <FormField
+                        control={form.control}
+                        name="place"
+                        render={({ field }) => (
+                            <FormItem className="mx-6">
+                            <FormLabel>Match location</FormLabel>
+                            <FormControl>
+                                <Input placeholder="Enter the match location" {...field} autoComplete="place"/>
+                            </FormControl>
+                            <FormDescription>
+                                This is your match location.
+                            </FormDescription>
+                            <FormMessage />
+                            </FormItem>
+                        )}
+                        />
+                        <FormField
+                        control={form.control}
+                        name="date"
+                        render={({ field }) => (
+                            <FormItem className="mx-6">
+                            <FormLabel className="mx-4">Match date</FormLabel>
+                            <FormControl>
+                                <DatePicker {...field} />
+                            </FormControl>
+                            <FormDescription>
+                                Choose your match date.
+                            </FormDescription>
+                            <FormMessage />
+                            </FormItem>
+                        )}
+                        />
+                    </form>
+                 </Form>
+                <DialogFooter>
+                    <Button type="submit">Save changes</Button>
+                <DialogClose asChild>
+                    <Button type="button" variant="secondary">
+                        Close
+                    </Button>
+                </DialogClose>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    );
+}
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,9 +2,14 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { useNavigate } from "react-router-dom";
 import { logOut, selectCurrentUser  } from '../features/auth/authSlice' // replace with the path to your auth slice
+import { CreateMatchDialog } from '../components/CreateMatchDialog';
+// shadcn componenets
 import { Card } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 import {  Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+
+
+
 
 const Dashboard = () => {
   const dispatch = useDispatch()
@@ -16,35 +21,56 @@ const Dashboard = () => {
   if (!username) {
     navigate("/login"); // or redirect to login page, or any other component you want to show while user is null
   }
+
   return (
-    <div className="flex flex-col h-screen">
-    <header className="p-4 bg-blue-900 text-white flex justify-between items-center">
-      <div className="font-bold text-xl">Dashboard</div>
-  
-      <div className="flex items-center">
-        <Card className="max-w-xs mx-4 bg-gray-800 hover:bg-gray-600 shadow-md 
-                         rounded px-4 pt-4 pb-4 mb-4 flex items-center cursor-pointer">
-          <Avatar className="w-8 h-8 rounded-full">
-            <AvatarImage src={avatar||undefined} alt="avatar" className="w-full h-full object-cover rounded-full" />
-            <AvatarFallback className="w-full h-full object-cover rounded-full bg-gray-700 text-gray-300">X</AvatarFallback>
-          </Avatar>
-          <div className="ml-2 text-gray-200 text-lg">{username}</div> 
-        </Card>
-  
-        <Button 
-          onClick={handleLogout} 
-          variant="destructive"
-          className="font-bold py-2 px-4 rounded"
-        >
-          Logout
-        </Button>
-      </div>
-    </header>
-  
-    <main className="flex-grow p-4 bg-gray-900 text-gray-200">
-      {/* Other content */}
-    </main>
+<div className="flex flex-col h-screen">
+  <header className="p-4  text-white flex justify-between items-center">
+    <div className="font-bold text-xl w-24 h-24 flex items-center justify-center">
+      <img className="w-full h-full object-cover" src="/public/pingMeetpong-logo.png" alt="Logo" /> 
+    </div>
+
+    <div className="flex items-center">
+      <Card className="max-w-xs mx-4 bg-gray-800 hover:bg-gray-600 shadow-md 
+                       rounded px-4 pt-4 pb-4 mb-4 flex items-center cursor-pointer">
+        <Avatar className="w-8 h-8 rounded-full">
+          <AvatarImage src={avatar||undefined} alt="avatar" className="w-full h-full object-cover rounded-full" />
+          <AvatarFallback className="w-full h-full object-cover rounded-full bg-gray-700 text-gray-300">X</AvatarFallback>
+        </Avatar>
+        <div className="ml-2 text-gray-200 text-lg">{username}</div> 
+      </Card>
+
+      <Button 
+        onClick={handleLogout} 
+        variant="destructive"
+        className="font-bold py-2 px-4 rounded"
+      >
+        Logout
+      </Button>
+    </div>
+  </header>
+
+  <main className="flex-grow p-4  text-gray-200 flex justify-between items-center">
+  <Card className="w-1/4 mx-4 bg-gray-800 hover:bg-gray-600 shadow-md 
+                 rounded px-4 pt-4 pb-4 mb-4 flex items-center cursor-pointer">
+    <div className="text-gray-200 text-lg">Matches</div> 
+  </Card>
+
+  <div className="flex flex-col items-center">
+
+
+  <CreateMatchDialog />
+
+    <Button className="font-bold py-2 px-4 rounded">
+      Join Match
+    </Button>
   </div>
+
+  <Card className="w-1/4 mx-4 bg-gray-800 hover:bg-gray-600 shadow-md 
+                 rounded px-4 pt-4 pb-4 mb-4 flex items-center cursor-pointer">
+    <div className="text-gray-200 text-lg">Friends</div> 
+  </Card>
+</main>
+</div>
   )
 }
 
