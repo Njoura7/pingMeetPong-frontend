a39e1ae996458a429fa5ed5fa57956f839e7efec
Njoura7 <njourawebdev@gmail.com>
2024-12-22 22:32:43 +0100

    hotfix: add not found route, lazy loading, and add _redirects for netlify

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,40 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 
 import './App.css'
-import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
-//pages
-import Register from './pages/Register';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard'
-import UserProfile from './pages/UserProfile';
+import { Suspense, lazy } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+
+import NotFound from './pages/NotFound';
 
 import { Provider } from 'react-redux';
-import { store } from './app/store'; 
+import { store } from './app/store';
 
-import {ToastContainer} from 'react-toastify'
+import { ToastContainer } from 'react-toastify'
 import "react-toastify/dist/ReactToastify.css"
 
 function App() {
 
-
+  const Register = lazy(() => import('./pages/Register'));
+  const Login = lazy(() => import('./pages/Login'));
+  const Dashboard = lazy(() => import('./pages/Dashboard'));
+  const UserProfile = lazy(() => import('./pages/UserProfile'));
 
   return (
 
     <Provider store={store}>
-    <BrowserRouter>
-      <ToastContainer/>
-        <Routes>
-          <Route path="/" element={<Navigate to="/register" replace/>}/>
-          <Route path="/register" element={<Register/>} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/dashboard" element={<Dashboard/>} />
-          <Route path="/profile/:userId" element={<UserProfile />} />
-        </Routes>
-    </BrowserRouter>
-  </Provider>
+      <BrowserRouter>
+        <ToastContainer />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Navigate to="/register" replace />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/profile/:userId" element={<UserProfile />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    </Provider >
   )
 }
 
